7a74b0eff95117333a8ead4c806147afbcc0431a
Tanaka Kuniaki <kuni1100001@gmail.com>
2021-05-05 15:00:37 +0900

    error の解消(.eslintrc.js)のnode:trueとruleを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
   env: {
+    node: true,
     browser: true,
     es6: true
   },
   extends: [
-    "eslint:recommended",
+    "eslint:recommended", //esリントのおすすめ初期設定が反映
     "plugin:@typescript-eslint/recommended", // TypeScriptでチェックされる項目をLintから除外する設定
     "prettier", // prettierのextendsは他のextendsより後に記述する
     // "prettier/@typescript-eslint",
@@ -16,5 +17,7 @@ module.exports = {
     "project": "./tsconfig.json" // TypeScriptのLint時に参照するconfigファイルを指定
   },
   root: true, // 上位ディレクトリにある他のeslintrcを参照しないようにする
-  rules: {}// TSでチェックしない様にしたい物を記載
+  rules: {
+    '@typescript-eslint/no-var-requires': 0
+  }// TSでチェックしない様にしたい物を記載
 }
